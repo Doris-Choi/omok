7f1fd6f27ba7f19b7d5be96af453e851d3784b65
doris <dychoi9298@gmail.com>
2020-09-06 18:11:14 +0900

    ✌ diagonal, anti-diagonal 방향 승리 판정 함수 올바른 기능으로 수정

--- index.js
+++ index.js
@@ -1,38 +1,197 @@
-const gomokuBorad = (() => {
-  let bw = 1;
+let player = 1; // player1: 1; player2: 2;
+let boardArr = [];
 
-  // 바둑판 그리기
-  const board = document.getElementById('board');
+const landing = document.querySelector('.landing');
+const game = document.getElementById('game');
+const board = document.getElementById('board');
+const btnRestart = document.querySelector('.btn-group button');
+const playerBox = document.querySelectorAll('.player-box');
+const totalGame = document.querySelector('.scoreboard h2 span');
+
+window.onload = () => {
+  const btnGameStart = document.querySelector('.landing button');
+  btnGameStart.addEventListener('click', (e) => {
+    landing.style.display = 'none';
+    render();
+  });
+};
+
+const render = () => {
+  const players = [
+    document.getElementById('player1').value || 'PLAYER 1',
+    document.getElementById('player2').value || 'PLAYER 2',
+  ];
+  for (let i = 0; i < 2; i++) {
+    const ele = playerBox[i].firstElementChild;
+    ele.innerText = players[i];
+    const stone = document.createElement('div');
+    stone.classList.add(i === 0 ? 'black' : 'white');
+    ele.appendChild(stone);
+  }
+  game.style.display = 'block';
+  renderGomokuBoard();
+};
+// 바둑판 그리기
+const renderGomokuBoard = () => {
+  // 초기화
+  boardArr = [];
+  board.innerHTML = '';
+
   const frag = document.createDocumentFragment();
+  let num = 0; // 바둑판 위치
   for (let i = 0; i < 19; i++) {
     const row = document.createElement('div');
     row.classList.add('row');
+    const sub = [];
     for (let j = 0; j < 19; j++) {
+      sub.push(0);
       const cell = document.createElement('div');
+      cell.innerText = num;
       cell.classList.add('cell');
-      cell.addEventListener('click', (e) => {
-        putStone(e);
-      });
+      cell.addEventListener('click', putStone);
       row.appendChild(cell);
+      num++;
     }
+    boardArr.push(sub);
     frag.appendChild(row);
   }
   board.appendChild(frag);
-
-  // 바둑돌 두기
-  const putStone = (e) => {
-    if (
-      e.target.classList.contains('black') ||
-      e.target.classList.contains('white')
-    ) {
-      return;
-    }
-    if (bw) {
-      e.target.classList.add('black');
-      bw -= 1;
+};
+// 바둑돌 두기
+const putStone = (e) => {
+  if (
+    e.target.classList.contains('black') ||
+    e.target.classList.contains('white')
+  ) {
+    return;
+  }
+  const r = parseInt(parseInt(e.target.innerText) / 19);
+  const c = parseInt(e.target.innerText) % 19;
+  boardArr[r][c] = player;
+  // fix
+  if (player === 1) {
+    e.target.classList.add('black');
+    decideWin(e);
+    player += 1;
+  } else if (player == 2) {
+    e.target.classList.add('white');
+    decideWin(e);
+    player -= 1;
+  }
+};
+// 승리 판단
+const decideWin = (e) => {
+  const r = parseInt(parseInt(e.target.innerText) / 19);
+  const c = parseInt(e.target.innerText) % 19;
+  if (
+    vertical(r, c) ||
+    horizontal(r, c) ||
+    diagonal(r, c) ||
+    antiDiagonal(r, c)
+  ) {
+    // 게임 기능 무력화
+    const cells = document.querySelectorAll('.cell');
+    [].forEach.call(cells, (cell) => {
+      cell.removeEventListener('click', putStone);
+    });
+    // score board 숫자 변경
+    totalGame.innerText = parseInt(totalGame.innerText) + 1;
+    playerBox[player - 1].lastElementChild.innerText;
+    alert(`${player === 1 ? '흑돌' : '백돌'}이 승리하였습니다.`);
+  }
+};
+const vertical = (r, c) => {
+  let count = 0;
+  let start = r - 4 >= 0 ? r - 4 : 0;
+  let end = r + 4 <= 18 ? r + 4 : 18;
+  while (start <= end) {
+    if (boardArr[start++][c] === player) {
+      count++;
+      if (count === 5) return true;
     } else {
-      e.target.classList.add('white');
-      bw += 1;
+      count = 0;
     }
-  };
-})();
+  }
+  return false;
+};
+const horizontal = (r, c) => {
+  let count = 0;
+  let start = c - 4 >= 0 ? c - 4 : 0;
+  let end = c + 4 <= 18 ? c + 4 : 18;
+  while (start <= end) {
+    if (boardArr[r][start++] === player) {
+      count++;
+      if (count === 5) return true;
+    } else {
+      count = 0;
+    }
+  }
+  return false;
+};
+const diagonal = (r, c) => {
+  let count = 0;
+  let startRow;
+  let startCol;
+  if (r === 0 || c === 0) {
+    startRow = r;
+    startCol = c;
+  } else if (r - 1 === 0 || c - 1 === 0) {
+    startRow = r - 1;
+    startCol = c - 1;
+  } else if (r - 2 === 0 || c - 2 === 0) {
+    startRow = r - 2;
+    startCol = c - 2;
+  } else if (r - 3 === 0 || c - 3 === 0) {
+    startRow = r - 3;
+    startCol = c - 3;
+  } else {
+    startRow = r - 4;
+    startCol = c - 4;
+  }
+  let endRow = r + 4 <= 18 ? r + 4 : 18;
+  let endCol = c + 4 <= 18 ? c + 4 : 18;
+  while (startRow <= endRow && startCol <= endCol) {
+    if (boardArr[startRow++][startCol++] === player) {
+      count++;
+      if (count === 5) return true;
+    } else {
+      count = 0;
+    }
+  }
+  return false;
+};
+const antiDiagonal = (r, c) => {
+  let count = 0;
+  let startRow;
+  let startCol;
+  if (r === 0 || c === 18) {
+    startRow = r;
+    startCol = c;
+  } else if (r - 1 === 0 || c + 1 === 18) {
+    startRow = r - 1;
+    startCol = c + 1;
+  } else if (r - 2 === 0 || c + 2 === 18) {
+    startRow = r - 2;
+    startCol = c + 2;
+  } else if (r - 3 === 0 || c + 3 === 18) {
+    startRow = r - 3;
+    startCol = c + 3;
+  } else {
+    startRow = r - 4;
+    startCol = c + 4;
+  }
+  console.log(startRow, startCol);
+  let endRow = r + 4 <= 18 ? r + 4 : 18;
+  let endCol = c - 4 >= 0 ? c - 4 : 0;
+  while (startRow <= endRow && startCol >= endCol) {
+    if (boardArr[startRow++][startCol--] === player) {
+      count++;
+      if (count === 5) return true;
+    } else {
+      count = 0;
+    }
+  }
+  return false;
+};
+
+btnRestart.addEventListener('click', renderGomokuBoard);
